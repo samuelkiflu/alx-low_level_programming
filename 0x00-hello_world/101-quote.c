4f8be5dffe393de0d34a49b8a781a5ac7a6d8d8d
samuelkiflu <samuelkiflu@gmail.com>
2023-07-13 11:36:43 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ * main- uses the unistd.h header file for write() function
+ * Return: 1
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
+
+
