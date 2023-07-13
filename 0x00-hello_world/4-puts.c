124a3bb993968c35f7f4abc259b241fba5dfe3b4
samuelkiflu <samuelkiflu@gmail.com>
2023-07-13 09:47:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Description:  a C program that prints exactly "Programming is  like building a multilingual puzzle. 
+ * The main function runs and it returns a zero.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
