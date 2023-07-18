1c4ba0de60336cca8887b934cc6a94f3f1b66b69
samuelkiflu <samuelkiflu@gmail.com>
2023-07-18 06:03:21 -0700

    betty style 3

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,13 @@
 /**
  * main- takes no parameters
  * _putchar - writes the character c to stdout
- * Return: returns 0 
+ * Return: returns 0
  */
-
 int main(void)
 {
-  char message[] = "_putchar\n";
- 
-  write(1, message, sizeof(message) - 1);
-  return (0);
+    char message[] = "_putchar\n";
+
+    write(1, message, sizeof(message) - 1);
+    return (0);
 }
+~
