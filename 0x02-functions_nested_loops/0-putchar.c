0c2b400d6745047b3dd6d858fe60082174a5ad53
samuelkiflu <samuelkiflu@gmail.com>
2023-07-18 05:40:19 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main- takes no parameters
+ * _putchar - writes the character c to stdout
+ *  Return: returns 0 
+ */
+
+int main(void)
+{
+    char message[] = "_putchar\n";
+    write(1, message, sizeof(message) - 1);
+    return 0;
+}
