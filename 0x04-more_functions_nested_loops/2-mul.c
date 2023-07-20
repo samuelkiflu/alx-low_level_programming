5e80c65b7b7f4e095def37f440fb36ca3a693247
samuelkiflu <samuelkiflu@gmail.com>
2023-07-20 16:47:42 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - recives int a and b and multiplies them.
+ * @a: first int parameter
+ * @b: second int parameter
+ * Return: multiple of a and b.
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
