02f689c029a58634ac591aeacba5e2d1029b1274
samuelkiflu <samuelkiflu@gmail.com>
2023-07-20 16:58:33 -0700

    betty style applied

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers 0 to 9 followed by a new line.
- *
- * Return: nothing. it is void function
- */
+* print_numbers - prints the numbers 0 to 9 followed by a new line.
+*
+* Return: nothing. it is void function
+*/
 
 void print_numbers(void)
 {
- 	int c;
+	int c;
 
-	 for (c = 48; c < 58; c++)
- 	{
- 	_putchar(c);
- 	}
- 	_putchar('\n');
+	for (c = 48; c < 58; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
 }
