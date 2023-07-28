4912978bd8132d0bfece7d206fc2f18987018327
samuelkiflu <samuelkiflu@gmail.com>
2023-07-27 22:26:31 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number  a function that prints an integer
+ * @m: integer input parameter
+ * Return: nothing - void function
+ */
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+
+	if (m / 10 != 0)
+	{
+		print_number(m / 10);
+	}
+	_putchar((m % 10) + '0');
+}
