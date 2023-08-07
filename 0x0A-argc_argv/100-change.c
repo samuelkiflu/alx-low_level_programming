f958f4f20d0688c442c631a12718e324c7b74425
samuelkiflu <samuelkiflu@gmail.com>
2023-08-07 02:46:26 -0700

    edit betty style 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
-* main - prints the minimum number of coins 
+* main - prints the minimum number of coins
 * to make change for an amount of cents.
 * @argc: number of arguments
 * @argv: array of arguments
@@ -13,8 +13,8 @@
 int main(int argc, char *argv[])
 {
 	int num, j, result;
-
 	int coins[] = {25, 10, 5, 2, 1};
+
 	if (argc != 2)
 	{
 	printf("Error\n");
