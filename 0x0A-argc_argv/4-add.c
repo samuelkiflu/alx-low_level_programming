360da127df626bf24c26c1a31eec108f00bd333a
samuelkiflu <samuelkiflu@gmail.com>
2023-08-07 01:30:22 -0700

    modify 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -5,39 +5,45 @@
 /**
  * checker - checks for valid input
  * @argc: argument count
- * @i: counter for argv[]
- * @j: counter for argv[][]
  * @argv: argument vector
  * Return: (0) on success, (1) on failure
  */
-int checker(int argc, int i, unsigned int j, char *argv[])
+
+int checker(int argc, char *argv[])
 {
-	for (i = 1; i <= argc; i++)
-		for (j = 0; argv[i][j] != '\0' && j < strlen(argv[i]); j++)
-			if (isdigit(argv[i][j]) == 0)
-				return (1);
+	for (int i = 1; i < argc; i++)
+	{
+	for (unsigned int j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+		return (1);
+		}
+	}
+	}
 	return (0);
 }
+
 /**
  * main - adds all arguments together if they are digits.
  * @argc: argument count only accepts ints separated by spaces.
  * @argv: argument vector
  * Return: (0) on success, (1) on failure
  */
+
 int main(int argc, char *argv[])
 {
-	int result, i;
+	int result = 0;
 
-	result = 0;
-	if (checker(argc, 1, 0, argv) == 1)
+	if (argc < 2 || checker(argc, argv) == 1)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 1; i < argc; i++)
+	for (int i = 1; i < argc; i++)
+	{
 		result += atoi(argv[i]);
+	}
 	printf("%d\n", result);
 	return (0);
 }
-
-
