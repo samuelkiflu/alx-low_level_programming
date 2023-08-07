244a23b8c79cecb924803e0af487cb276f5031f2
samuelkiflu <samuelkiflu@gmail.com>
2023-08-07 00:44:04 -0700

    rewrite 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,21 +1,15 @@
-
 #include <stdio.h>
-#include "main.h"
-
 /**
-* main - prints all arguments it receives
-* @argc: number of arguments
-* @argv: array of arguments
-* Return: Always 0 (Success)
-*/
-
+ * main - prints out all of the arguments passed to it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: (0) on success
+ */
 int main(int argc, char *argv[])
 {
-int i;
+	printf("%d\n", argc - 1);
+	argv[0] = 0;
+	return (0);
+}
 
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
-return (0);
-}
+
