76dbb27aa6673abedf367759d41ad397078acd1c
samuelkiflu <samuelkiflu@gmail.com>
2023-08-09 04:40:56 -0700

    rewrite algorithm 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+
 /**
- * main - Prints the numbers from 0-9.
-(*
- * Return: 0
- */
+* main - Prints the numbers from 0-9.
+*
+* Return: 0
+*/
+
 int main(void)
 {
-	printf("0123456789\n");
+	int n = 0;
+
+	while (n < 10)
+	{
+	printf("%d", n);
+	n++;
+	}
+	printf("\n");
 	return (0);
 }
-
