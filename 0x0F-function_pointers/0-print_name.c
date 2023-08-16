eac758a54c0a90e774d123e541a47df419e79678
samuelkiflu <samuelkiflu@gmail.com>
2023-08-16 10:47:42 -0700

    0-print_name.c add if statement

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,9 +3,13 @@
   * print_name - a function that prints a name
   * @name: input string
   * @f: function pointer casted to char pointer
+  * Return- nothing
   */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
+
 	(*f)(name);
 }
 
